566c1ffef009b30b88a708c83772b0d7bf73f439
HendersonRodrigues <126429221+HendersonRodrigues@users.noreply.github.com>
2024-11-10 17:35:44 +0000

    base do codigo

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,30 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    
+    int atual = 0;
+    int tabuleiro[8][8];
+    int escolha = 0;
+    printf("Em nosso jogo de Xadrez, voce poderá mover o Bispo, a Torre ou a Rainha\n   Escolha uma peça:\n      1 - Bispo\n      2 - Torre\n      3 - Rainha\n");
+    scanf("%d", &escolha);
+    switch (escolha)
+    {
+    case 1:
+        printf ("A peça escolhida foi: Bispo\n");
+        Bispo();
+        break;
+    case 2:
+        printf ("A peça escolhida foi: Torre\n");
+        Torre();
+        break;
+    case 3:
+        printf ("A peça escolhida foi: Rainha\n");
+        Rainha();
+        break;
+
+    default:
+        break;
+    }
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
@@ -17,16 +41,28 @@ int main() {
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
+   
     return 0;
+}
+
+void Bispo(void){
+
+
+
+
+}
+
+void Torre(void){
+
+
+
+
+}
+
+void Rainha(void){
+
+
+
+
+
 }
